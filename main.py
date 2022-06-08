edcaf5b60cabafaa01e35a309a4f20472044db51
Ghouse <ghouse.temp@gmail.com>
2022-06-08 20:00:01 +0300

    Added Stock Data Insights Graph/Plot

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from turtle import color
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -17,6 +18,27 @@ end = dt.datetime(2022,1,1)
 
 sensex_data = web.DataReader("^BSESN",'yahoo',start ,end) ##Webscraping
 
+#print(sensex_data)
+#Data Cleaning
 
 
 
+sensex_data = sensex_data.drop_duplicates()
+
+#Checking Null Values
+#print(sensex_data.isnull().sum())
+
+#Statistical analysis
+print(sensex_data.describe())
+
+print(sensex_data)
+
+#Closing Price Graph
+plt.figure(figsize=(20,10))
+sns.lineplot(x = "Date", y = "Close" , data = sensex_data)
+plt.title("Closing price of SENSEX from 2001 to 2022", size = 15, color = "blue")
+plt.xticks(size = "15")
+plt.yticks(size = "15")
+plt.xlabel("Time in Years", size = 15)
+plt.ylabel("Closing Price", size = 15)
+plt.show()
